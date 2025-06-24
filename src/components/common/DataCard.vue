40a9fed1cf62026f479858078d87055d366b75c1
Future <hello.duan@icloud.com>
2025-06-24 11:00:30 +0800

    Update DataCard.vue

--- src/components/common/DataCard.vue
+++ src/components/common/DataCard.vue
@@ -21,11 +21,19 @@
     <div v-else class="space-y-2">
       <div class="flex justify-between">
         <span class="text-sm text-gray-600">今日采集</span>
-        <span :class="primaryTextClass" class="text-sm font-semibold">{{ data.todayCount }} 条</span>
+        <span :class="primaryTextClass" class="text-sm font-semibold">
+          <a href="javascript:;" @click="$emit('showRawData', 'today')" class="text-blue-600 underline hover:text-blue-800">
+            {{ data.todayCount }} 条
+          </a>
+        </span>
       </div>
       <div class="flex justify-between">
         <span class="text-sm text-gray-600">总计</span>
-        <span class="text-sm font-semibold">{{ data.totalCount }} 条</span>
+        <span class="text-sm font-semibold">
+          <a href="javascript:;" @click="$emit('showRawData', 'total')" class="text-blue-600 underline hover:text-blue-800">
+            {{ data.totalCount }} 条
+          </a>
+        </span>
       </div>
       <div class="flex justify-between">
         <span class="text-sm text-gray-600">成功率</span>
