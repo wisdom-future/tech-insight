a49f382ec03a202763bbb02b181921536aec43a6
Future <hello.duan@icloud.com>
2025-06-24 19:57:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/tech-insight/',
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
+      '@': '/src'
     }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
   }
 })
