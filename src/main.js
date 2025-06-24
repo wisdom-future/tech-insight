e75a35dd26cb8f5731ab734a8971930761041dee
Future <hello.duan@icloud.com>
2025-06-24 19:57:38 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/styles/main.css'
-
-console.log('Starting Vue application...')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(ElementPlus)
 app.mount('#app')
-
-console.log('Vue application mounted successfully')
